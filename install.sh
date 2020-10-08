758c8696c7a75cc7afa84e65deea8a4fdaf549a8
Owen Ou <o@owenou.com>
2020-10-07 20:38:48 -0700

    Clean up install script

--- install.sh
+++ install.sh
@@ -21,7 +21,6 @@ main() {
 
   get_architecture || return 1
   local _arch="$RETVAL"
-  assert_nz "$_arch" "arch"
 
   local _ext=""
   case "$_arch" in
@@ -69,10 +68,6 @@ check_cmd() {
   command -v "$1" > /dev/null 2>&1
 }
 
-assert_nz() {
-  if [ -z "$1" ]; then err "assert_nz $2"; fi
-}
-
 # Run a command that should never fail. If the command fails execution
 # will immediately terminate with an error showing the failing
 # command.
@@ -110,22 +105,6 @@ downloader() {
   else
     err "Unknown downloader"   # should not reach here
   fi
-}
-
-# Return strong TLS 1.2-1.3 cipher suites in OpenSSL or GnuTLS syntax. TLS 1.2 
-# excludes non-ECDHE and non-AEAD cipher suites. DHE is excluded due to bad 
-# DH params often found on servers (see RFC 7919). Sequence matches or is
-# similar to Firefox 68 ESR with weak cipher suites disabled via about:config.  
-# $1 must be openssl or gnutls.
-get_strong_ciphersuites_for() {
-  if [ "$1" = "openssl" ]; then
-    # OpenSSL is forgiving of unknown values, no problems with TLS 1.3 values on versions that don't support it yet.
-    echo "TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384"
-  elif [ "$1" = "gnutls" ]; then
-    # GnuTLS isn't forgiving of unknown values, so this may require a GnuTLS version that supports TLS 1.3 even if wget doesn't.
-    # Begin with SECURE128 (and higher) then remove/add to build cipher suites. Produces same 9 cipher suites as OpenSSL but in slightly different order.
-    echo "SECURE128:-VERS-SSL3.0:-VERS-TLS1.0:-VERS-TLS1.1:-VERS-DTLS-ALL:-CIPHER-ALL:-MAC-ALL:-KX-ALL:+AEAD:+ECDHE-ECDSA:+ECDHE-RSA:+AES-128-GCM:+CHACHA20-POLY1305:+AES-256-GCM"
-  fi 
 }
 
 get_architecture() {
@@ -183,10 +162,9 @@ get_architecture() {
 
       esac
 
-      _arch="${_ostype}-${_cputype}"
+  _arch="${_ostype}-${_cputype}"
 
-      RETVAL="$_arch"
-    }
+  RETVAL="$_arch"
+}
 
-
-  main "$@" || exit 1
+main "$@" || exit 1
