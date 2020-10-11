f83359eebd8b4b3a3e9b90d00e32fa532c23f26e
Owen Ou <o@owenou.com>
2020-10-10 19:14:20 -0700

    Update tag-release

--- bin/util.sh
+++ bin/util.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
 version() {
-  ver_file="internal/commands/version.go"
   ver="$(git describe --tags HEAD 2>/dev/null || true)"
 
   if [ -z "$ver" ]; then
-    ver="$(grep -w 'Version =' "$ver_file" | cut -d'"' -f2)"
+    ver="$(grep -w 'Version =' "$version_file" | cut -d'"' -f2)"
     sha="$(git rev-parse --short HEAD 2>/dev/null || true)"
     [ -z "$sha" ] || ver="${ver}-g${sha}"
   fi
@@ -13,3 +12,4 @@ version() {
   echo "${ver#v}"
 }
 
+version_file="internal/commands/version.go"
